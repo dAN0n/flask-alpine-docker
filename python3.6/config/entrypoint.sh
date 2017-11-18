61f523bb1d6f4539831dfdb8d31140a4d40e0b10
Zobkov Dmitry <dan0na@ya.ru>
2017-11-18 10:21:30 +0300

    Moved ENVs from Dockerfile to entrypoint.sh

--- python3.6/config/entrypoint.sh
+++ python3.6/config/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
-# Get the maximum upload file size for Nginx, default to 0: unlimited
-USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
-# Set maximum upload file size
-sed -i -E "s/(client_max_body_size) 1m;/\1 $USE_NGINX_MAX_UPLOAD;/" /etc/nginx/nginx.conf
 
-# Get the URL for static files from the environment variable
-USE_STATIC_URL=${STATIC_URL:-'/static'}
-# Get the absolute path of the static files from the environment variable
-USE_STATIC_PATH=${STATIC_PATH:-'/app/static'}
+# By default, allow unlimited file sizes, modify it to limit the file sizes
+NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
+# URL under which static (not modified by Python) files will be requested
+# They will be served by Nginx directly, without being handled by uWSGI
+STATIC_URL=${STATIC_URL:-'/static'}
+# Absolute path in where the static files will be
+STATIC_PATH=${STATIC_PATH:-'/app/static'}
+# If STATIC_INDEX is 1, serve / with /static/index.html directly (or the static URL configured)
+STATIC_INDEX=${STATIC_INDEX:-0}
+
+# Set maximum upload file size
+sed -i -E "s/(client_max_body_size) 1m;/\1 $NGINX_MAX_UPLOAD;/" /etc/nginx/nginx.conf
 
 # Generate Nginx config first part using the environment variables
 echo 'server {
@@ -19,14 +23,14 @@ echo 'server {
         include uwsgi_params;
         uwsgi_pass unix:///tmp/uwsgi.sock;
     }
-    '"location $USE_STATIC_URL {
-        alias $USE_STATIC_PATH;
+    '"location $STATIC_URL {
+        alias $STATIC_PATH;
     }" > /etc/nginx/conf.d/nginx.conf
 
 # If STATIC_INDEX is 1, serve / with /static/index.html directly (or the static URL configured)
 if [[ $STATIC_INDEX == 1 ]] ; then 
 echo "    location = / {
-        index $USE_STATIC_URL/index.html;
+        index $STATIC_URL/index.html;
     }" >> /etc/nginx/conf.d/nginx.conf
 fi
 # Finish the Nginx config file
